d94c2cff2c924756db6805e0e3016681dd910281
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-07-11 15:20:45 -0700

    Start distutils for Python DFXML libraries
    This patch starts distutils functionality by making something
    installable in site-packages.
    
    I left the 'scripts' argument in commented out, to show that it places
    scripts where we might not want.  See the second snippet, installing to
    ./tmp2, below.
    
    .../python$ python setup.py install --prefix=$PWD/tmp1
    [snip]
    .../python$ find tmp1
    tmp1
    tmp1/lib
    tmp1/lib/python2.7
    tmp1/lib/python2.7/site-packages
    tmp1/lib/python2.7/site-packages/dfxml-1.0.1-py2.7.egg-info
    tmp1/lib/python2.7/site-packages/dfxml.py
    tmp1/lib/python2.7/site-packages/dfxml.pyc
    tmp1/lib/python2.7/site-packages/fiwalk.py
    tmp1/lib/python2.7/site-packages/fiwalk.pyc
    
    It's from the next snippet that I suppose the autotools would be better
    for listing installable scripts.
    
    .../python$ python setup.py install --prefix=$PWD/tmp2
    [snip]
    .../python$ find tmp2
    tmp2
    tmp2/bin
    tmp2/bin/idifference.py
    tmp2/bin/rdifference.py
    tmp2/lib
    tmp2/lib/python2.7
    tmp2/lib/python2.7/site-packages
    tmp2/lib/python2.7/site-packages/dfxml-1.0.1-py2.7.egg-info
    tmp2/lib/python2.7/site-packages/dfxml.py
    tmp2/lib/python2.7/site-packages/dfxml.pyc
    tmp2/lib/python2.7/site-packages/fiwalk.py
    tmp2/lib/python2.7/site-packages/fiwalk.pyc

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+  name='dfxml',
+  version='1.0.1',
+  url='https://github.com/simsong/dfxml',
+#  scripts=['idifference.py','rdifference.py'],
+  py_modules=['dfxml','fiwalk']
+)
