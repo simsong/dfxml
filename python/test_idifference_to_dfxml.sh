217a658961f643150c771c325972f91f3ef649c9
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-09-17 17:15:07 -0400

    idifference: Add validated change counts to unit test
    Signed-off-by: Alex Nelson <a.nelson@prometheuscomputing.com>

--- python/test_idifference_to_dfxml.sh
+++ python/test_idifference_to_dfxml.sh
@@ -22,3 +22,7 @@ fi
 
 #Check expected number of fileobjects appears
 test 4 == $(grep '<fileobject' idifference_test_formatted.dfxml | wc -l)
+test 1 == $(grep 'delta:new_file' idifference_test_formatted.dfxml | wc -l)
+test 1 == $(grep 'delta:deleted_file' idifference_test_formatted.dfxml | wc -l)
+test 2 == $(grep 'delta:changed_file' idifference_test_formatted.dfxml | wc -l)
+test 7 == $(grep 'delta:changed_property' idifference_test_formatted.dfxml | wc -l)
