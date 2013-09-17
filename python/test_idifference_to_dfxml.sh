710975c8676aa7b9f81afd57d9d6e9e91640bdfd
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-09-17 12:29:17 -0400

    idifference: Test text report and fileobject tally
    Unfortunately, this test shows the fileobjects aren't being generated
    anymore.

--- python/test_idifference_to_dfxml.sh
+++ python/test_idifference_to_dfxml.sh
@@ -9,9 +9,16 @@ set -e
 #Display all executed commands
 set -x
 
+#Ensure the non-XML output doesn't fail, first.
+"$PYTHON3" idifference.py --summary ../samples/difference_test_[01].xml > idifference_test.txt
+
+#Generate XML output.
 "$PYTHON3" idifference.py --xml idifference_test.dfxml ../samples/difference_test_[01].xml
 if [ -x "$XMLLINT" ]; then
   xmllint --format idifference_test.dfxml >idifference_test_formatted.dfxml
 else
   echo "Warning: xmllint not found.  Skipped check for if generated DFXML is valid XML." >&2
 fi
+
+#Check expected number of fileobjects appears
+test 4 == $(grep '<fileobject' idifference_test_formatted.dfxml | wc -l)
