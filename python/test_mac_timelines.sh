6113566a359396277f34b20b0a87a464a9b3ecc8
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-09-18 11:34:19 -0400

    Python: Run timeline test on differencing input XML
    Also implemented a known-good line count check.
    
    Signed-off-by: Alex Nelson <a.nelson@prometheuscomputing.com>

--- python/test_mac_timelines.sh
+++ python/test_mac_timelines.sh
@@ -7,7 +7,20 @@ set -e
 #Display all executed commands
 set -x
 
-"$PYTHON2" demo_mac_timeline.py ../samples/simple.xml
-"$PYTHON3" demo_mac_timeline.py ../samples/simple.xml
-"$PYTHON2" demo_mac_timeline_iter.py ../samples/simple.xml
-"$PYTHON3" demo_mac_timeline_iter.py ../samples/simple.xml
+"$PYTHON2" demo_mac_timeline.py ../samples/simple.xml >demo_mac_timeline_simple_p2.txt
+test 12 == $(cat demo_mac_timeline_simple_p2.txt | wc -l)
+
+"$PYTHON3" demo_mac_timeline.py ../samples/simple.xml >demo_mac_timeline_simple_p3.txt
+test 12 == $(cat demo_mac_timeline_simple_p3.txt | wc -l)
+
+"$PYTHON2" demo_mac_timeline_iter.py ../samples/simple.xml >demo_mac_timeline_iter_simple_p2.txt
+test 12 == $(cat demo_mac_timeline_iter_simple_p2.txt | wc -l)
+
+"$PYTHON3" demo_mac_timeline_iter.py ../samples/simple.xml >demo_mac_timeline_iter_simple_p3.txt
+test 12 == $(cat demo_mac_timeline_iter_simple_p3.txt | wc -l)
+
+"$PYTHON3" demo_mac_timeline.py ../samples/difference_test_1.xml >demo_mac_timeline_dt1.txt
+test 9 == $(cat demo_mac_timeline_dt1.txt | wc -l)
+
+"$PYTHON3" demo_mac_timeline_iter.py ../samples/difference_test_1.xml >demo_mac_timeline_iter_dt1.txt
+test 9 == $(cat demo_mac_timeline_iter_dt1.txt | wc -l)
