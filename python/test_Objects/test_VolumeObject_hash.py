590aa93ff94368b45234cffa14a1587e97ea31d1
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-11-14 16:58:23 -0500

    Start Object tests directory

---
+++ python/test_Objects/test_VolumeObject_hash.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import Objects
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+s0 = set()
+
+v0 = Objects.VolumeObject()
+v1 = Objects.VolumeObject()
+
+s0.add(v0)
+s0.add(v1)
+
+logging.debug("len(s0) = %r" % len(s0))
+assert len(s0) == 2
+
+f0 = Objects.FileObject()
+f1 = Objects.FileObject()
+f0.volume_object = v0
+f1.volume_object = v0
+
+s1 = set()
+s1.add(f0.volume_object)
+s1.add(f1.volume_object)
+logging.debug("len(s1) = %r" % len(s1))
+assert len(s1) == 1
