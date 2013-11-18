92ff787126b012ad53c88750e984ed26fce75293
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-11-18 14:55:09 -0500

    Add debug printing

--- python/test_differential_dfxml.sh
+++ python/test_differential_dfxml.sh
@@ -2,6 +2,6 @@
 
 source _pick_pythons.sh
 
-"$PYTHON3" make_differential_dfxml.py ../samples/difference_test_[01].xml > differential_dfxml_test.xml
+"$PYTHON3" make_differential_dfxml.py -d ../samples/difference_test_[01].xml > differential_dfxml_test.xml
 
 "$PYTHON3" summarize_differential_dfxml.py -d differential_dfxml_test.xml > differential_dfxml_test.txt
