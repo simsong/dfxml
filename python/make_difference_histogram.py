2f6fefeaab6b97cfb9dfbae3cfb2366f15dbb84f
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-11-24 16:49:17 -0500

    Jot down note to make a difference histogram program

---
+++ python/make_difference_histogram.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This program (-to-be) reads a DFXML file with differential annotations and produces a table.
+
+Columns: FileObject annotation.
+Rows: Counts of instances of a property being changed per FileObject annotation.  One row per FileObject direct-child element.
+"""
+
+__version__ = "0.0.0"
+
+raise NotImplementedError("TODO")
