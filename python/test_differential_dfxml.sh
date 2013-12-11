2cc60cff8bc8afdf769fafa7f4b9af565e65b07c
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-12-11 16:44:32 -0500

    Format generated XML

--- python/test_differential_dfxml.sh
+++ python/test_differential_dfxml.sh
@@ -2,10 +2,10 @@
 
 source _pick_pythons.sh
 
-"$PYTHON3" make_differential_dfxml.py -d ../samples/difference_test_{0,1}.xml > differential_dfxml_test_01.xml
+"$PYTHON3" make_differential_dfxml.py -d ../samples/difference_test_{0,1}.xml | xmllint --format - > differential_dfxml_test_01.xml
 
 "$PYTHON3" summarize_differential_dfxml.py -d differential_dfxml_test_01.xml > differential_dfxml_test_01.txt
 
-"$PYTHON3" make_differential_dfxml.py -d ../samples/difference_test_{2,3}.xml > differential_dfxml_test_23.xml
+"$PYTHON3" make_differential_dfxml.py -d ../samples/difference_test_{2,3}.xml | xmllint --format - > differential_dfxml_test_23.xml
 
 "$PYTHON3" summarize_differential_dfxml.py -d differential_dfxml_test_23.xml > differential_dfxml_test_23.txt
