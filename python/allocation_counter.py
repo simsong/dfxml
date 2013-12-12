6184ea0b0edfe0bd37e9d6efe597cdda28ab4f96
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-12-12 16:16:49 -0500

    Add forgotten arguments

--- python/allocation_counter.py
+++ python/allocation_counter.py
@@ -4,7 +4,7 @@
 For a disk image or DFXML file, this program produces a cross-tabulation of the allocation state of each file's inode and name.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 #Version 0.2.0:
 # * Tabular output in HTML
 # * Tabular output in LaTeX
@@ -23,9 +23,9 @@ _logger = logging.getLogger(os.path.basename(__file__))
 def main():
     counter = collections.defaultdict(lambda: 0)
     prev_obj = None
-    for (event, obj) in Objects.iterparse(sys.argv[1]):
+    for (event, obj) in Objects.iterparse(args.input_image):
         if isinstance(obj, Objects.FileObject):
-            if make_differential_dfxml.ignorable_name(obj.filename):
+            if args.ignore_virtual_files and make_differential_dfxml.ignorable_name(obj.filename):
                 continue
             counter[(obj.alloc_inode, obj.alloc_name)] += 1
 
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ignore-virtual-files", action="store_true", help="Use the same file-ignoring rules as make_differential_dfxml.py.")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug printing.")
+    parser.add_argument("input_image", help="Disk image, or DFXML file.")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
