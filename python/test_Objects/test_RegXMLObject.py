4f5d00a834fd4cb0a923543fee4d025e93c94993
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-08-18 16:44:05 -0400

    Use minor version

--- python/test_Objects/test_RegXMLObject.py
+++ python/test_Objects/test_RegXMLObject.py
@@ -3,7 +3,7 @@ import Objects
 import test_diffing_CellObject
 import test_diffing_HiveObject
 
-ro = Objects.RegXMLObject(version="2.0")
+ro = Objects.RegXMLObject(version="0.2")
 ho = Objects.HiveObject()
 ho.append(test_diffing_CellObject.co)
 ho.append(test_diffing_CellObject.nco)
