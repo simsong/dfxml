c6e38aca6140f5bee0eb34595185762a2937de16
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-08-21 18:33:27 -0400

    OS X: Restore explicit Python 3 executable listing
    The Python 3 binaries installed by MacPorts aren't readily picked up by
    'which'.
    
    Signed-off-by: Alex Nelson <a.nelson@prometheuscomputing.com>

--- python/_pick_pythons.sh
+++ python/_pick_pythons.sh
@@ -2,8 +2,17 @@
 # This script is meant to be included in Bash scripts that need a Python v2 and v3.
 # An autotool configure script would also suffice.
 PYTHON2=`which python`
-PYTHON3=`which python3`
+PYTHON3=`which python3.4`
 if [ -z "$PYTHON3" ]; then
-  echo "Error: Could not find a python3 executable." >&2
-  exit 1
+  PYTHON3=`which python3.3`
+  if [ -z "$PYTHON3" ]; then
+    PYTHON3=`which python3.2`
+    if [ -z "$PYTHON3" ]; then
+      PYTHON3=`which python3`
+      if [ -z "$PYTHON3" ]; then
+        echo "Error: Could not find a python3 executable." >&2
+        exit 1
+      fi
+    fi
+  fi
 fi
