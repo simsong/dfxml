87d51b64f187097906919339cb0d1142d60204f7
Alex Nelson <a.nelson@prometheuscomputing.com>
2015-01-21 14:33:03 -0500

    Prevent accidental error conditions on missing a Python 3 version
    Signed-off-by: Alex Nelson <a.nelson@prometheuscomputing.com>

--- python/_pick_pythons.sh
+++ python/_pick_pythons.sh
@@ -1,14 +1,15 @@
 
 # This script is meant to be included in Bash scripts that need a Python v2 and v3.
 # An autotool configure script would also suffice.
+# The 'or echo' statements keep the subshell from returning an error exit status on missing a Python version.
 PYTHON2=`which python`
-PYTHON3=`which python3.4`
+PYTHON3=`which python3.4 || echo`
 if [ -z "$PYTHON3" ]; then
-  PYTHON3=`which python3.3`
+  PYTHON3=`which python3.3 || echo`
   if [ -z "$PYTHON3" ]; then
-    PYTHON3=`which python3.2`
+    PYTHON3=`which python3.2 || echo`
     if [ -z "$PYTHON3" ]; then
-      PYTHON3=`which python3`
+      PYTHON3=`which python3 || echo`
       if [ -z "$PYTHON3" ]; then
         echo "Error: Could not find a python3 executable." >&2
         exit 1
