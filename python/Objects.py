f3dcf40f120f9a34c2fc11d7ea1f4529d81a4852
Alex Nelson <alexander.nelson@nist.gov>
2018-02-28 17:40:58 -0500

    Objects.py: Handle numeric casting and st_ino in Python 2 on Windows 7
    A user reported a bad attempt to cast `long` values on Python 2.  The
    user encountered `st_ino` with a value of `0L`.  As I was implementing a
    fix for `st_ino`, I encountered some files that also had long sizes.
    Thus, the fix is two parts:
    
    * Have `_intcast(x)` recognize and preserve the `long` type in Python 2.
    * Since Python 2 in Windows only reports `0` for `st_ino`, treat this as
      null information.  Python 3's handling of `st_ino` may be worth future
      attention.
    
    This fix appears to run fine in Windows, though with only light testing.
    The test methodology was to run `walk_to_dfxml.py` in the `dfxml/python`
    source tree; the whole unit test suite wasn't run for lack of Make on my
    test Windows machine.  Unit tests pass in macOS.
    
    This resolves [Issue 29](https://github.com/simsong/dfxml/issues/29).
    
    Reported-by: Kieran O'Leary <kieran.o.leary@gmail.com>
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- python/Objects.py
+++ python/Objects.py
@@ -18,7 +18,7 @@ This file re-creates the major DFXML classes with an emphasis on type safety, se
 With this module, reading disk images or DFXML files is done with the parse or iterparse functions.  Writing DFXML files can be done with the DFXMLObject.print_dfxml function.
 """
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 #Remaining roadmap to 1.0.0:
 # * Documentation.
@@ -34,6 +34,7 @@ import dfxml
 import os
 import sys
 import struct
+import platform
 
 _logger = logging.getLogger(os.path.basename(__file__))
 
@@ -106,6 +107,9 @@ def _intcast(val):
     """Casts input integer or string to integer.  Preserves nulls.  Balks at everything else."""
     if val is None:
         return None
+    if sys.version_info[0] < 3:
+        if isinstance(val, long):
+            return val
     if isinstance(val, int):
         return val
 
@@ -2148,12 +2152,19 @@ class FileObject(object):
         _typecheck(s, os.stat_result)
 
         self.mode = s.st_mode
-        self.inode = s.st_ino
         self.nlink = s.st_nlink
         self.uid = s.st_uid
         self.gid = s.st_gid
         self.filesize = s.st_size
         #s.st_dev is ignored for now.
+
+        if platform.system() == "Windows":
+            # On Windows, Python 2 reports 0L.  Treat this as absent information.
+            # On Windows, Python 3 reports the "File ID" ( see "nFileIndexLow" remark at: https://msdn.microsoft.com/en-us/library/aa363788 ).  Record this as the inode number for now.  NOTE: in the future this may become a Windows-namespaced property "fileindex"; it may be prudent to later file a follow-on to Python Issue 32878 ( https://bugs.python.org/issue32878 ).
+            if sys.version_info[0] >= 3:
+                self.inode = s.st_ino
+        else:
+            self.inode = s.st_ino
 
         if "st_mtime" in dir(s):
             self.mtime = s.st_mtime
