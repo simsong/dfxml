6f46ece4010ee1f4d60a4df4afaf48e8bc4b4c77
Simson Garfinkel <simson.l.garfinkel@census.gov>
2019-05-07 17:14:30 -0400

    fixed line ends; will need to fix all of the others. Ugh.

--- python/dfxml/tests/FileObject_byte_run_facets_test.py
+++ python/dfxml/tests/FileObject_byte_run_facets_test.py
@@ -1,4 +1,3 @@
-
 # This software was developed at the National Institute of Standards
 # and Technology in whole or in part by employees of the Federal
 # Government in the course of their official duties. Pursuant to
