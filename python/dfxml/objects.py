cccdbf6d3bcadba925653b4f5c5324afe14a4022
Alex Nelson <alexander.nelson@nist.gov>
2019-12-02 10:37:38 -0500

    objects.py: Add parser transition for partition system without containing disk image
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- python/dfxml/objects.py
+++ python/dfxml/objects.py
@@ -4284,6 +4284,7 @@ class Parser(object):
         _DFXML_END,
         _DFXML_METADATA_START,
         _DISK_IMAGE_START,
+        _PARTITION_SYSTEM_START,
         _VOLUME_START,
         _FILE_START,
         DFXML_POSTSTREAM
