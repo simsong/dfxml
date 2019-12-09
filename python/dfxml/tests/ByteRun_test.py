0dfc4163c88d88aa09a6b53bc8f68d7c207b8e3f
Alex Nelson <alexander.nelson@nist.gov>
2019-12-09 12:24:51 -0500

    objects.py tests: Test glomming of contiguous byte runs
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- python/dfxml/tests/ByteRun_test.py
+++ python/dfxml/tests/ByteRun_test.py
@@ -25,19 +25,6 @@ import dfxml.objects as Objects
 
 _logger = logging.getLogger(os.path.basename(__file__))
 
-def test_img_offset_len():
-    br1 = Objects.ByteRun()
-    br1.img_offset = 512
-    br1.len = 20
-
-    br2 = copy.deepcopy(br1)
-
-    assert br1 + br2 is None
-
-    br2.img_offset += br1.len
-
-    assert (br1 + br2).len == 40
-
 def test_fill():
     _logger = logging.getLogger(os.path.basename(__file__))
     logging.basicConfig(level=logging.DEBUG)
@@ -70,3 +57,91 @@ def test_fill():
     br.fill = 1
     _logger.debug("br.fill = %r." % br.fill)
     assert br.fill == b'\x01'
+
+def _gen_glom_samples(offset_property="img_offset"):
+    """
+    Generate three contiguous byte runs.
+    """
+    br0 = Objects.ByteRun()
+    br1 = Objects.ByteRun()
+    br2 = Objects.ByteRun()
+
+    br0.len = 20
+    br1.len = 30
+    br2.len = 10
+
+    if offset_property == "img_offset":
+        br0.img_offset = 0
+        br1.img_offset = 20
+        br2.img_offset = 50
+    elif offset_property == "fs_offset":
+        br0.fs_offset = 0
+        br1.fs_offset = 20
+        br2.fs_offset = 50
+    elif offset_property == "file_offset":
+        br0.file_offset = 0
+        br1.file_offset = 20
+        br2.file_offset = 50
+
+    return (br0, br1, br2)
+
+def test_glomming_simple():
+    for offset_property in [
+      "img_offset",
+      "fs_offset",
+      "file_offset"
+    ]:
+        (br0, br1, br2) = _gen_glom_samples(offset_property)
+        br0_br0      = br0 + br0
+        br0_br1      = br0 + br1
+        br0_br2      = br0 + br2
+        br1_br0      = br1 + br0
+        br1_br2      = br1 + br2
+
+        try:
+            assert br0_br0 is None
+        except:
+            _logger.debug("br0_br0 = %r." % br0_br0)
+            raise
+
+        try:
+            assert getattr(br0_br1, offset_property) == 0
+            assert br0_br1.len == 50
+        except:
+            _logger.debug("br0_br1 = %r." % br0_br1)
+            raise
+
+        try:
+            assert br0_br2 is None
+        except:
+            _logger.debug("br0_br2 = %r." % br0_br2)
+            raise
+
+        try:
+            assert br1_br0 is None
+        except:
+            _logger.debug("br1_br0  = %r." % br1_br0 )
+            raise
+
+        try:
+            assert getattr(br1_br2, offset_property) == 20
+            assert br1_br2.len == 40
+        except:
+            _logger.debug("br1_br2 = %r." % br1_br2)
+            raise
+
+        try:
+            br0__br1_br2 = br0 + br1_br2
+            assert getattr(br0__br1_br2, offset_property) == 0
+            assert br0__br1_br2.len == 60
+        except:
+            _logger.debug("br0__br1_br2 = %r." % br0__br1_br2)
+            raise
+
+        try:
+            br0_br1__br2 = br0_br1 + br2
+            assert getattr(br0_br1__br2, offset_property) == 0
+            assert br0_br1__br2.len == 60
+        except:
+            _logger.debug("br0_br1__br2 = %r." % br0_br1__br2)
+            raise
