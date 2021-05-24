4ce03ceed53ac014265260cff0efb6a516be534b
Simson L. Garfinkel <simsong@acm.org>
2021-05-24 17:27:59 -0400

    Slg dev (#65)
    * minor change
    
    * added
    
    * added file
    
    * more updates for C++17
    
    * minor reformatting
    
    * updated .gitignore
    
    * Update dfxml_writer.h
    
    * updated to dfxml_writer.h with no dfxml_writer.cpp

--- src/dfxml_writer.h
+++ src/dfxml_writer.h
@@ -516,6 +516,9 @@ public:
 
     const std::string &get_outfilename() const {return outfilename; } ;
 
+    /********************************
+     *** THESE ARE ALL THREADSAFE ***
+     ********************************/
     void add_rusage() {
 #ifdef WIN32
         /* Note: must link -lpsapi for this */
@@ -609,7 +612,6 @@ public:
         *out << "\n";
         out->flush();
     }
-
 
 
     /* These all call xmlout or xmlprintf which already has locking, so these are all threadsafe! */
