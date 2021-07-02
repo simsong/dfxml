4182dafb30bafd82c992683357bf7388fe90f181
Alex Nelson <alexander.nelson@nist.gov>
2021-07-02 15:25:46 -0400

    Move to new repositories (#72)

--- python/dfxml/conftest.py
+++
@@ -1,4 +0,0 @@
-#
-# This file is empty, but it permits test discovery of the subdirectory.
-# See:
-# https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada
--- python/tests/paths.sh
+++
@@ -1,3 +0,0 @@
-source _pick_pythons.sh
-
-DEMO_DIR=../demos
